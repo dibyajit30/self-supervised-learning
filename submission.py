58d7ace785ea194f5e53e120ffbf8ca8c40c6072
dibyajit <dibyajit30@gmail.com>
2021-04-09 17:57:19 -0400

    Common code for CPU, GPU

--- submission.py
+++ submission.py
@@ -2,9 +2,9 @@
 
 from torchvision import models, transforms
 
-team_id = 1
-team_name = "your_team_name"
-email_address = "team_leader_nyu_email_address@nyu.edu"
+team_id = 16
+team_name = "Integer"
+email_address = "sg6148@nyu.edu"
 
 def get_model():
     return models.resnet18(num_classes=800)
